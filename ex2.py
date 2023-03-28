c9ae80a2ca4fe2492e1d138f338e2fa76dd3d0bb
unknown <kolahola149@gmail.com>
2023-03-28 19:25:33 +0300

    start linear-regression

--- ex2.py
+++ ex2.py
@@ -2,16 +2,11 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 
-data = pd.read_csv("ex2.csv", encoding="Windows-1251")
+data = pd.read_csv("ex2_1.csv", encoding="Windows-1251")
 
 model = PCA(svd_solver='full')
-model.fit(data)
-
-first_pc = model.components_[0]
-second_pc = model.components_[1]
-
+model = model.fit(data)
 transformed_data = model.transform(data)
+
 print(model.explained_variance_ratio_)
-print(np.dot(transformed_data[0], first_pc))
-print(np.dot(transformed_data[0], second_pc))
-# for i, j in zip(transformed_data, data):
+print(transformed_data[0])
--- readme.md
+++ readme.md
@@ -8,3 +8,14 @@ Take one line and then calculate angle by scalar product. Lines should have angl
 
 ## Preparation
 Find mean value each column, than minus from each one, so we get centralized values F. Then we have to find one vector x with length equal to 1 (Z = Fx). Find x and remember, that Z should be max and |x|=1. 
+
+
+# Linear regression
+
+## Motivation 
+The Method main components is difficult in usage, so better option is linear regression. Based on multiple on one koeficent.
+
+## How calculate main koeficents
+y(x) = O1*x + O0
+O1 = S((Yi-Ym)*(Xi-Xm))/S((Xi-Xm)^2)
+O0 = Ym - O1*Xm
